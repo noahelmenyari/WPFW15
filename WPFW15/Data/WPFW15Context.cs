e75b5eb71d1c23343291abb586cf2dc249a12521
Noah Menyari <noah.menyari@gmail.com>
2020-12-20 17:53:39 +0100

    Add project files.

---
+++ WPFW15/Data/WPFW15Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WPFW15.Models;
+
+namespace WPFW15.Data
+{
+    public class WPFW15Context : DbContext
+    {
+        public WPFW15Context (DbContextOptions<WPFW15Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WPFW15.Models.Student> Student { get; set; }
+    }
+}
---
+++ WPFW15/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WPFW15.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+    }
+}
